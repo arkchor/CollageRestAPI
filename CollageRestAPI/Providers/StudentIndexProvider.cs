4266a98f180ec44d273eab650fa6d577c38dd4ac
Arek <arkadiusz.chorian@gmail.com>
2016-04-23 17:48:03 +0200

    fully working MongoDB

--- CollageRestAPI/Providers/StudentIndexProvider.cs
+++ CollageRestAPI/Providers/StudentIndexProvider.cs
@@ -8,23 +8,48 @@ using System.Web;
 
 namespace CollageRestAPI.Providers
 {
-    public static class StudentIndexProvider
+    public class StudentIndexProvider
     {
-        //private static readonly Lazy<StudentIndexProvider> lazy =
-        //new Lazy<StudentIndexProvider>(() => new StudentIndexProvider());
-        //public static StudentIndexProvider Instance { get { return lazy.Value; } }
-        //private StudentIndexProvider() { }
-
-        public static int currentIndex;
-        public static int GetNewIndex()
+        private static readonly Lazy<StudentIndexProvider> lazy =
+        new Lazy<StudentIndexProvider>(() => new StudentIndexProvider());
+        public static StudentIndexProvider Instance { get { return lazy.Value; } }
+        private StudentIndexProvider()
         {
-            return ++currentIndex;
+            SetCurrentIndex();
         }
-        public static void SetCurrentIndex()
+
+        private int _currentIndex;
+
+        public int currentIndex
         {
-            var col = BaseRepository.Instance.Db.GetCollection<BsonDocument>("studentindexprovider");
-            System.Diagnostics.Debug.WriteLine(col.Find(new BsonDocument()).FirstOrDefault().ToString());
-            System.Diagnostics.Debug.WriteLine(col.Find(new BsonDocument()).FirstOrDefault().Elements.Single(x => x.Name == "currentIndex").Value);
+            get
+            {
+                _currentIndex++;
+                System.Diagnostics.Debug.WriteLine("======111========= " + _currentIndex + " ========111=======");
+                var collection = BaseRepository.Instance.Db.GetCollection<BsonDocument>("studentindexprovider");
+                //var filter = Builders<BsonDocument>.Filter.
+                var update = Builders<BsonDocument>.Update.Set("currentIndex", _currentIndex);
+                collection.UpdateOne(Builders<BsonDocument>.Filter.Empty, update);
+                //collection.AsQueryable().Single().
+                //collection.AsQueryable().Single().Set("currentIndex", _currentIndex);
+                //collection.Find(new BsonDocument()).FirstOrDefault().Set("currentIndex", currentIndex);
+                return _currentIndex;
+            }
+            set { _currentIndex = value; }
+        }
+        
+        //public int GetNewIndex()
+        //{
+        //    return ++currentIndex;
+        //}
+        private void SetCurrentIndex()
+        {
+            var collection = BaseRepository.Instance.Db.GetCollection<BsonDocument>("studentindexprovider");
+            _currentIndex = collection.AsQueryable().Single().Elements.Single(x => x.Name == "currentIndex").Value.ToInt32();
+            System.Diagnostics.Debug.WriteLine("================== " + _currentIndex + " ==================");
+            //currentIndex = collection.Find(new BsonDocument()).FirstOrDefault().Elements.Single(x => x.Name == "currentIndex").Value.ToInt32();
+            //System.Diagnostics.Debug.WriteLine(col.Find(new BsonDocument()).FirstOrDefault().ToString());
+            //System.Diagnostics.Debug.WriteLine(col.Find(new BsonDocument()).FirstOrDefault().Elements.Single(x => x.Name == "currentIndex").Value);
             //currentIndex = BaseRepository.Instance.Db.GetCollection("")
         }
     }
--- CollageRestAPI/Startup.cs
+++ CollageRestAPI/Startup.cs
@@ -7,6 +7,8 @@ using MongoDB.Driver;
 using CollageRestAPI.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
+using CollageRestAPI.Providers;
+using System.Collections;
 
 [assembly: OwinStartup(typeof(CollageRestAPI.Startup))]
 
@@ -17,6 +19,7 @@ namespace CollageRestAPI
         public void Configuration(IAppBuilder app)
         {
             DbInit();
+            DbTest();
             ConfigureAuth(app);
         }
 
@@ -26,13 +29,37 @@ namespace CollageRestAPI
             BsonClassMap.RegisterClassMap<StudentModel>(x => x.AutoMap());
             BsonClassMap.RegisterClassMap<CourseModel>(x => x.AutoMap());
             BsonClassMap.RegisterClassMap<GradeModel>(x => x.AutoMap());
+            BsonClassMap.RegisterClassMap<StudentIndexProvider>(x => x.AutoMap());
 
             var dbClient = BaseRepository.Instance.DbClient;
-            var db = BaseRepository.Instance.Db;
+            //var db = BaseRepository.Instance.Db;
             dbClient = new MongoClient("mongodb://localhost");
-            db = dbClient.GetDatabase("collagerestapi");          
-            //db.RunCommandAsync((Command<BsonDocument>)"{ping:1}").Wait();
-            //System.Diagnostics.Debug.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            BaseRepository.Instance.Db = dbClient.GetDatabase("collagerestapi");
+            BaseRepository.Instance.Db.RunCommandAsync((Command<BsonDocument>)"{ping:1}").Wait();
+            System.Diagnostics.Debug.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        }
+
+        private void DbTest()
+        {
+            System.Diagnostics.Debug.WriteLine("XXXXXXXXXXXXXXXXX    DB TEST  IN  XXXXXXXXXXXXXXXXXXXXX");
+            //StudentIndexProvider.Instance.SetCurrentIndex();
+            List<StudentModel> students = new List<StudentModel>();
+            for (int i = 0; i < 5; i++)
+            {
+                var student = new StudentModel();
+                student.FirstName = "imie" + i;
+                student.LastName = "nazwisko" + i;
+                student.Index = StudentIndexProvider.Instance.currentIndex;
+                students.Add(student);
+            }
+            BaseRepository.Instance.Students.AddRange(students);
+            var db = BaseRepository.Instance.Db;
+            //if(db.ListCollections().ForEachAsync(x => x.))
+            //var list = db.ListCollections();
+            //if(db.GetCollection("students").)
+            var collection = db.GetCollection<StudentModel>("students");//.InsertMany(BaseRepository.Instance.Students);
+            collection.InsertMany(students);
+            System.Diagnostics.Debug.WriteLine("XXXXXXXXXXXXXXXXX    DB TEST  OUT XXXXXXXXXXXXXXXXXXXXX");
         }
     }
 }
