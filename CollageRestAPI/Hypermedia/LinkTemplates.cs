33d2279b837dc7fe7ef63de80c02a21fea9c26cd
Arek <arkadiusz.chorian@gmail.com>
2016-04-29 08:56:20 +0200

    Link templates class added

---
+++ CollageRestAPI/Hypermedia/LinkTemplates.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Routing;
+
+namespace CollageRestAPI.Hypermedia
+{
+    public static class LinkTemplates
+    {
+        public static class Students
+        {
+            public static Link GetStudentByIdLink(UrlHelper urlHelper, int studentId)
+                => new Link("GetStudentById", urlHelper.Link("GetStudentById", new { id = studentId }), "GET");
+            public static Link GetStudentGradesLink(UrlHelper urlHelper, int studentId)
+                => new Link("GetStudentGrades", urlHelper.Link("GetStudentGrades", new { id = studentId }), "GET");
+            public static Link GetStudentCoursesLink(UrlHelper urlHelper, int studentId)
+                => new Link("GetStudentCourses", urlHelper.Link("GetStudentCourses", new { id = studentId }), "GET");
+
+            public static Link CreateStudentLink(UrlHelper urlHelper, int studentId)
+                => new Link("CreateStudent", urlHelper.Link("CreateStudent", new { id = studentId }), "POST");
+
+            public static Link UpdateStudentByIdLink(UrlHelper urlHelper, int studentId)
+                => new Link("UpdateStudentById", urlHelper.Link("UpdateStudentById", new { id = studentId }), "PUT");
+
+            public static Link DeleteStudentByIdLink(UrlHelper urlHelper, int studentId)
+                => new Link("DeleteStudentById", urlHelper.Link("DeleteStudentById", new { id = studentId }), "DELETE");
+        }
+
+        public static class Courses
+        {
+
+        }
+    }
+}
